1060cb06691dbd4e191690d131578bf29c5b9070
Jan Drugowitsch <jdrugo@gmail.com>
2019-05-16 17:38:19 -0400

    Remove Figure S6 files

--- figS6/sat.m
+++
@@ -1,9 +0,0 @@
-function col = sat(r, colEd, colSt)
-% function col = sat(r, colEd, colSt)
-
-if ~exist('colEd','var'); colEd = [0 0 0]; end
-if ~exist('colSt','var'); colSt = [.9 .9 .9]; end
-
-for iR = 1:length(r)
-    col(iR,:) = r(iR) * colEd + (1-r(iR)) * colSt;
-end
