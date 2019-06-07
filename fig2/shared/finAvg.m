4bcd6a239e2f4d5341d9e81b61701f4205ffc54c
Jan Drugowitsch <jdrugo@gmail.com>
2019-06-07 10:01:18 -0400

    Move fig2/shared to shared

--- fig2/shared/finAvg.m
+++
@@ -1,7 +0,0 @@
-function a = finAvg(X,dim)
-% function a = finAvg(X,[dim=1])
-if nargin < 2;  dim = 1;  end
-
-Xfin = isfinite(X);
-X(~Xfin) = 0;
-a = sum(X,dim)./sum(Xfin, dim);
--- fig2/shared/subplotXY.m
+++
@@ -1,2 +0,0 @@
-function h = subplotXY(yMax, xMax, y, x)
-h = subplot(yMax, xMax, xMax*(y-1)+x);
